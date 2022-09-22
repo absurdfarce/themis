a51a48ae36d1fc6b4f284ec8a3f385c9bcb21bbc
Bret McGuire <bret.mcguire@datastax.com>
2022-09-22 16:49:24 -0500

    Removing old code

--- app/src/main/java/com/datastax/themis/ThemisException.java
+++
@@ -1,8 +0,0 @@
-package com.datastax.themis;
-
-public class ThemisException extends RuntimeException {
-
-    public ThemisException(String msg) { super(msg); }
-
-    public ThemisException(String msg, Throwable cause) { super(msg, cause); }
-}
