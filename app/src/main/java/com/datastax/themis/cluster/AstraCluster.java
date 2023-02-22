4483f0326cac69a36bc5b35698ef421faded9742
Bret McGuire <bret.mcguire@datastax.com>
2023-02-22 14:46:07 -0600

    Move to clusters should be mostly done now

--- app/src/main/java/com/datastax/themis/cluster/AstraCluster.java
+++ app/src/main/java/com/datastax/themis/cluster/AstraCluster.java
@@ -1,23 +1,82 @@
 package com.datastax.themis.cluster;
 
 import com.datastax.oss.driver.api.core.CqlSession;
-import com.datastax.themis.config.ClusterConfig;
+import com.datastax.themis.ThemisException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.util.Optional;
 
 public class AstraCluster {
 
-    private final String name;
-    private final ClusterConfig config;
+    private static Logger logger = LoggerFactory.getLogger(AstraCluster.class);
 
-    public AstraCluster(String name, ClusterConfig config) {
-        this.name = name;
-        this.config = config;
+    private final Path scb;
+    private final String username;
+    private final String password;
+
+    private AstraCluster(Path scb, String username, String password) {
+        this.scb = scb;
+        this.username = username;
+        this.password = password;
     }
+
+    public AstraCluster.Builder builder() { return new AstraCluster.Builder(); }
 
     CqlSession buildSession() {
         return CqlSession
                 .builder()
-                .withCloudSecureConnectBundle(this.config.getScb().get())
-                .withAuthCredentials(this.config.getUsername().get(), this.config.getPassword().get())
+                .withCloudSecureConnectBundle(this.scb)
+                .withAuthCredentials(this.username, this.password)
                 .build();
+    }
+
+    public static class Builder {
+
+        private Optional<Path> scb = Optional.empty();
+        private Optional<String> username = Optional.empty();
+        private Optional<String> password = Optional.empty();
+
+        public Builder scb(Path scb) {
+            this.scb = Optional.of(scb);
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = Optional.of(username);
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = Optional.of(password);
+            return this;
+        }
+
+        private boolean validate() {
+
+            if (this.scb.isEmpty()) {
+                logger.error("Secure connect bundle (SCB) is required for DefaultCluster");
+                return false;
+            }
+            if (this.username.isEmpty()) {
+                logger.error("Username (Astra client ID) is required for AstraCluster");
+                return false;
+            }
+            if (this.password.isEmpty()) {
+                logger.error("Password (Astra secret) is required for AstraCluster");
+                return false;
+            }
+            return true;
+        }
+
+        public AstraCluster build()
+                throws ThemisException {
+
+            if (! validate()) {
+                throw new ThemisException("Could not validate configs for Astra cluster, consult the log for details");
+            }
+            return new AstraCluster(this.scb.get(),this.username.get(), this.password.get());
+        }
     }
 }
--- app/src/main/java/com/datastax/themis/cluster/DefaultCluster.java
+++ app/src/main/java/com/datastax/themis/cluster/DefaultCluster.java
@@ -2,31 +2,105 @@ package com.datastax.themis.cluster;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.CqlSessionBuilder;
-import com.datastax.themis.config.ClusterConfig;
+import com.datastax.themis.ThemisException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.Optional;
 
 public class DefaultCluster extends Cluster {
 
-    private final String name;
-    private final ClusterConfig config;
+    private static Logger logger = LoggerFactory.getLogger(DefaultCluster.class);
 
-    public DefaultCluster(String name, ClusterConfig config) {
-        this.name = name;
-        this.config = config;
+    private final InetAddress address;
+    private final int port;
+    private final String localDc;
+    private final Optional<String> username;
+    private final Optional<String> password;
+
+    private DefaultCluster(InetAddress address, int port, String localDc, Optional<String> username, Optional<String> password) {
+        this.address = address;
+        this.port = port;
+        this.localDc = localDc;
+        this.username = username;
+        this.password = password;
     }
+
+    public Builder builder() { return new Builder(); }
 
     CqlSession buildSession() {
 
         CqlSessionBuilder builder = CqlSession
                 .builder()
-                .addContactPoint(new InetSocketAddress(this.config.getAddress().get(), this.config.getPort().get()))
-                .withLocalDatacenter(this.config.getLocalDc().get());
-        this.config.getUsername().ifPresent(u -> {
-            this.config.getPassword().ifPresent(p -> {
+                .addContactPoint(new InetSocketAddress(this.address, this.port))
+                .withLocalDatacenter(this.localDc);
+        this.username.ifPresent(u -> {
+            this.password.ifPresent(p -> {
                 builder.withAuthCredentials(u,p);
             });
         });
         return builder.build();
+    }
+
+    public static class Builder {
+
+        private Optional<InetAddress> address = Optional.empty();
+        private Optional<Integer> port = Optional.empty();
+        private Optional<String> localDc = Optional.empty();
+        private Optional<String> username = Optional.empty();
+        private Optional<String> password = Optional.empty();
+
+        public Builder address(InetAddress address) {
+            this.address = Optional.of(address);
+            return this;
+        }
+
+        public Builder port(int port) {
+            this.port = Optional.of(port);
+            return this;
+        }
+
+        public Builder localDc(String localDc) {
+            this.localDc = Optional.of(localDc);
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = Optional.of(username);
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = Optional.of(password);
+            return this;
+        }
+
+        private boolean validate() {
+
+            if (this.address.isEmpty()) {
+                logger.error("Address is required for DefaultCluster");
+                return false;
+            }
+            if (this.port.isEmpty()) {
+                logger.error("Port is required for DefaultCluster");
+                return false;
+            }
+            if (this.localDc.isEmpty()) {
+                logger.error("Local DC is required for DefaultCluster");
+                return false;
+            }
+            return true;
+        }
+
+        public DefaultCluster build()
+        throws ThemisException {
+
+            if (! validate()) {
+                throw new ThemisException("Could not validate configs for default cluster, consult the log for details");
+            }
+            return new DefaultCluster(this.address.get(),this.port.get(), this.localDc.get(), this.username, this.password);
+        }
     }
 }
