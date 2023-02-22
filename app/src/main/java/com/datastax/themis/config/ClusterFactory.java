f26f709bb583007ba60cf05426bf3af81e654e14
Bret McGuire <bret.mcguire@datastax.com>
2023-02-22 13:57:00 -0600

    Work in progress, saving before refactoring clusters

---
+++ app/src/main/java/com/datastax/themis/config/ClusterFactory.java
@@ -0,0 +1,12 @@
+package com.datastax.themis.config;
+
+import com.datastax.themis.cluster.Cluster;
+
+import java.util.Map;
+
+public class ClusterFactory {
+
+    public static Cluster buildCluster(Map<String,?> clusterConfig) {
+
+    }
+}
--- app/src/main/java/com/datastax/themis/config/ConfigLoader.java
+++ app/src/main/java/com/datastax/themis/config/ConfigLoader.java
@@ -1,8 +1,6 @@
 package com.datastax.themis.config;
 
-import com.datastax.themis.ThemisException;
 import com.datastax.themis.cluster.Cluster;
-import com.datastax.themis.cluster.DefaultCluster;
 import com.google.common.collect.ImmutableMap;
 import org.yaml.snakeyaml.Yaml;
 
@@ -11,37 +9,19 @@ import java.util.Map;
 
 public class ConfigLoader {
 
-    public static ImmutableMap<ClusterName, Cluster> load(InputStream in)
-    throws ThemisException {
+    public static ImmutableMap<ClusterName, Cluster> loadAllClusters(InputStream in) {
 
         Yaml yaml = new Yaml();
         Map<String, Map<String, ?>> data = yaml.load(in);
-        return buildClusters(data);
-    }
 
-    private static ImmutableMap<ClusterName, Cluster> buildClusters(Map<String,Map<String,?>> config)
-    throws ThemisException {
-
-        ImmutableMap<ClusterName, Map<String,?>> data = convertConfigMap(config);
-
+        /* It's okay if we don't have an entry in the YAML for every cluster... we'll just try to work with what we have */
         ImmutableMap.Builder<ClusterName, Cluster> rvBuilder = ImmutableMap.builder();
-        for (ClusterName name : ClusterName.values()) {
-            if (! data.containsKey(name)) {
-                throw new ThemisException(String.format("Config must contain top-level key %s", name));
-            }
-            rvBuilder.put(name,
-                    new DefaultCluster(
-                            name.name(),
-                            ClusterConfig.buildFromConfig(name, convertClusterMap(data.get(name)))));
+        for (Map.Entry<String, Map<String, ?>> entry : data.entrySet()) {
+
+            rvBuilder.put(
+                    ClusterName.valueOf(entry.getKey()),
+                    ClusterFactory.buildCluster(entry.getValue()));
         }
         return rvBuilder.build();
-    }
-
-    private static <T> ImmutableMap<ClusterName, T> convertConfigMap(Map<String, T> input) {
-        return input.entrySet().stream().collect(ImmutableMap.toImmutableMap(e -> ClusterName.valueOf(e.getKey().toUpperCase()), e -> e.getValue()));
-    }
-
-    private static <T> ImmutableMap<ClusterConfigKey, ?> convertClusterMap(Map<String, ?> input) {
-        return input.entrySet().stream().collect(ImmutableMap.toImmutableMap(e -> ClusterConfigKey.valueOf(e.getKey().toUpperCase()), e -> e.getValue()));
     }
 }
